291a67c8573e2e6db00dcc7473c9318db8c59c81
Dhruv kathiriya <150034575+Dhruv-Kathiriya@users.noreply.github.com>
2023-12-29 19:55:32 +0530

    task1.c

--- task1.c
+++ task1.c
@@ -13,23 +13,23 @@ Odd numbers from odd_file.txt:
 
 void main() {
     
-    FILE *even, *odd;
+    FILE *e, *o;
     int i;
 
-    even = fopen("even.txt", "w");
-    odd = fopen("odd.txt", "w");
+    e = fopen("even.txt", "w");
+    o = fopen("odd.txt", "w");
 
-    if(even == NULL || odd == NULL){
+    if(e == NULL || o == NULL){
         printf("can't open file\n");
     }
     for(i = 50; i <= 70; i++){
         if(i % 2 == 0){
-            fprintf(even, "%d\n", i);
+            fprintf(e,"%d\n", i);
         }else{
-            fprintf(odd, "%d\n", i);
+            fprintf(o,"%d\n", i);
         }
     }
 
-    printf("Successfully added even and odd numbers to files\n");
+    printf("Successfully added  numbers to files\n");
 
 }
